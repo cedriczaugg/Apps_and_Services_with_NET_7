d1b33594bd2508b0367dc882e233d417e0a71ba0
Cedric Zaugg <cedric.zaugg@eta.ch>
2023-05-12 18:59:08 +0200

    Reorganize files.

--- Chapter01/CodeAnalyzing/Program.cs
+++ Chapter01/CodeAnalyzing/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace CodeAnalyzing
+﻿// <copyright file="Program.cs" company="Packt">
+// Copyright (c) Packt. All rights reserved.
+// </copyright>
+
+using System.Diagnostics;
+
+namespace CodeAnalyzing;
+
+/// <summary>
+/// The main class for this console app.
+/// </summary>
+public class Program
 {
-    internal class Program
+    /// <summary>
+    /// The main entry point for this console app.
+    /// </summary>
+    /// <param name="args">
+    /// A string array of arguments passed to the console app.
+    /// </param>
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Debug.WriteLine("Hello, Debugger!");
     }
 }
