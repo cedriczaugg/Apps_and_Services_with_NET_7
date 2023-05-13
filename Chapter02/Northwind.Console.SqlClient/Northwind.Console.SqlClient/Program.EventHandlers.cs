45e3fa1e7668f67d2cc6a791f548301081697df7
Cedric Zaugg <cedric.zaugg123@gmail.com>
2023-05-13 20:19:07 +0200

    Chapter 2 create database

---
+++ Chapter02/Northwind.Console.SqlClient/Northwind.Console.SqlClient/Program.EventHandlers.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+internal partial class Program
+{
+    private static void Connection_StateChange(object sender, StateChangeEventArgs e)
+    {
+        var previousColor = ForegroundColor;
+        ForegroundColor = ConsoleColor.DarkYellow;
+        WriteLine($"State change from {e.OriginalState} to {e.CurrentState}.");
+        ForegroundColor = previousColor;
+    }
+
+    private static void Connection_InfoMessage(object sender, SqlInfoMessageEventArgs e)
+    {
+        var previousColor = ForegroundColor;
+        ForegroundColor = ConsoleColor.DarkBlue;
+        WriteLine($"Info: {e.Message}.");
+        foreach (SqlError error in e.Errors) WriteLine($"  Error: {error.Message}");
+        ForegroundColor = previousColor;
+    }
+}
---
+++ Chapter02/Northwind.Console.SqlClient/Northwind.Console.SqlClient/Program.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Data.SqlClient;
+
+// SqlConnection and so on
+SqlConnectionStringBuilder builder = new();
+builder.InitialCatalog = "Northwind";
+builder.MultipleActiveResultSets = true;
+builder.Encrypt = true;
+builder.TrustServerCertificate = true;
+builder.ConnectTimeout = 10;
+WriteLine("Connect to:");
+WriteLine("  1 - SQL Server on local machine");
+WriteLine("  2 - Azure SQL Database");
+WriteLine("  3 – Azure SQL Edge");
+WriteLine();
+Write("Press a key: ");
+var key = ReadKey().Key;
+WriteLine();
+WriteLine();
+
+switch (key)
+{
+    case ConsoleKey.D1 or ConsoleKey.NumPad1:
+        builder.DataSource = "."; // Local SQL Server
+        // @".\net7book"; // Local SQL Server with an instance name
+        break;
+    case ConsoleKey.D2 or ConsoleKey.NumPad2:
+        builder.DataSource = // Azure SQL Database
+            "tcp:apps-services-net7.database.windows.net,1433";
+        break;
+    case ConsoleKey.D3 or ConsoleKey.NumPad3:
+        builder.DataSource = "tcp:127.0.0.1,1433"; // Azure SQL Edge
+        break;
+    default:
+        WriteLine("No data source selected.");
+        return;
+}
+
+WriteLine("Authenticate using:");
+WriteLine("  1 – Windows Integrated Security");
+WriteLine("  2 – SQL Login, for example, sa");
+WriteLine();
+Write("Press a key: ");
+key = ReadKey().Key;
+WriteLine();
+WriteLine();
+switch (key)
+{
+    case ConsoleKey.D1 or ConsoleKey.NumPad1:
+        builder.IntegratedSecurity = true;
+        break;
+    case ConsoleKey.D2 or ConsoleKey.NumPad2:
+    {
+        builder.UserID = "sa"; // Azure SQL Edge
+        // "markjprice"; // change to your username
+        Write("Enter your SQL Server password: ");
+        var password = ReadLine();
+        if (string.IsNullOrWhiteSpace(password))
+        {
+            WriteLine("Password cannot be empty or null.");
+            return;
+        }
+
+        builder.Password = password;
+        builder.PersistSecurityInfo = false;
+        break;
+    }
+    default:
+        WriteLine("No authentication selected.");
+        return;
+}
+
+SqlConnection connection = new(builder.ConnectionString);
+WriteLine(connection.ConnectionString);
+WriteLine();
+connection.StateChange += Connection_StateChange;
+connection.InfoMessage += Connection_InfoMessage;
+try
+{
+    WriteLine("Opening connection. Please wait up to {0} seconds...",
+        builder.ConnectTimeout);
+    WriteLine();
+    connection.Open();
+    WriteLine($"SQL Server version: {connection.ServerVersion}");
+    connection.StatisticsEnabled = true;
+}
+catch (SqlException ex)
+{
+    WriteLine($"SQL exception: {ex.Message}");
+    return;
+}
+
+connection.Close();
